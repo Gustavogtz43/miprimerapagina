fea9b60a94b09efc8411cbcc114e4a4358426b4c
Gustavo Gutierrez <gustavoagm43@gmail.com>
2025-01-29 18:08:27 -0600

    Agregando los archivos iniciales de la pagina

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://github.com/Gustavogtz43/miprimerapagina',
+    base: 'miprimerapagina',
     integrations: [mdx(), sitemap(), tailwind()],
 });
