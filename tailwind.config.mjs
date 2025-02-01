34d2d108e681ef1c7a75482303320f9c03114e09
Gustavo Gutierrez <gustavoagm43@gmail.com>
2025-01-31 19:00:06 -0600

    Estilos de header, Hero and Hero2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],  // Agregar la fuente personalizada
+			  },
+			colors: { 
+				orangep: "#FDBF22",
+				gris: "#6D6D6D",
+				rojito: "#EB3F0C",
+				rojoclaro: "#FC7D3A",
+				oscurito: "#3D3D3D",
+				backgroundImage: {
+					'custom-icon': "url('/src/components/img.jpg')",
+		  		},
+			},
+		},
 	},
 	plugins: [],
 }
